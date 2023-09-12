b27cc6daf1cc3c983429fc480671dcc1761d1110
Brandon Do <brandondo7274@gmail.com>
2023-09-12 13:39:27 -0700

    sep12 140p

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+const App = () => {
+
+    return <div>brandon's solo proj</div>
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,33 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    // somehow knows its in src folder so dont need ./src/index.js
     output: {
         path: path.resolve(__dirname, 'public'),
+        // changed public from dist
         filename: 'main.js'
+        // don't have a main.js yet.
+        // will auto create once we npm run build
     },
 
-    // plugins: [
-    //     new HTMLWebpackPlugin({
-    //         template: './src/index/html'
-    //     })
-    // ],
-
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.jsx?/
-    //             // /.js$/
-    //         }
-    //     ]
-    // }
-
+    target: 'web',
+    devServer: {
+        port: '3000',
+        static: ['./public'],
+        open: true,
+        hot: true,
+        liveReload: true
+    }, 
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+        ],
+    },
 }
