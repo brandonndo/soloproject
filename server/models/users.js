f95b67bc864083837d289724f34395b79a0e03cc
Brandon Do <brandondo7274@gmail.com>
2023-09-12 19:26:23 -0700

    mongo compass

---
+++ server/models/users.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const UsersSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    
+})
--- server/server.js
+++ server/server.js
@@ -3,11 +3,16 @@ const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
 
+mongoose.connect("mongodb+srv://brandondo:q29o00G4he70xi48@cluster0.arxquxp.mongodb.net/bdoSoloProject?retryWrites=true&w=majority")
 
 // const PORT = 3000;
 
 app.listen(3000, () => {
-    console.log(`Server listening!`);
+    console.log(`SERVER RUNNING!`);
 });
 
 module.exports = app;
+
+
+
+// mongo : brandondo, passs q29o00G4he70xi48
